6945d6d2c31b9b02ac51c74305410b334e793f91
stories2 <stories2@naver.com>
2017-08-14 10:12:20 +0900

    DataTransfer module : structure added

---
+++ Utils/DataTransfer.js
@@ -0,0 +1,15 @@
+function DataTransferManager() {
+
+}
+
+DataTransferManager.prototype.CustomJsonToOriginalJson = function (customJsonData) {
+    
+} 
+
+DataTransferManager.prototype.CommunicateWithBackend = function (originalJsonRequestData, callbackReciveObject) {
+    
+}
+
+DataTransferManager.prototype.JsonToGetParameters = function (originalJsonRequestData) {
+    
+}
