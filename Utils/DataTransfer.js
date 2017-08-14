920620774500b1e3258f51ecc344061f2b287d7d
stories2 <stories2@naver.com>
2017-08-14 10:39:27 +0900

    DataTransfer module : convert json to get parameters

--- Utils/DataTransfer.js
+++ Utils/DataTransfer.js
@@ -6,10 +6,21 @@ DataTransferManager.prototype.CustomJsonToOriginalJson = function (customJsonDat
     
 } 
 
-DataTransferManager.prototype.CommunicateWithBackend = function (originalJsonRequestData, callbackReciveObject) {
+DataTransferManager.prototype.CommunicateWithBackend = function (url, originalJsonRequestData, callbackReciveObject) {
     
 }
 
 DataTransferManager.prototype.JsonToGetParameters = function (originalJsonRequestData) {
-    
+    var getParameters = ""
+
+    for (key in originalJsonRequestData) {
+        if (originalJsonRequestData.hasOwnProperty(key)) {
+            getParameters = getParameters + key + "=" + originalJsonRequestData[key] + "&"
+        }
+        else {
+            PrintLogMessage("DataTransfer", "JsonToGetParameters", "not available key", LOG_LEVEL_WARN)
+        }
+    }
+    PrintLogMessage("DataTransfer", "JsonToGetParameters", "converted get: " + getParameters, LOG_LEVEL_INFO)
+    return getParameters
 }
