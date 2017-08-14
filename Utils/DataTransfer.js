9d1a5d1ed4ee0da81921426b38c6890b201fbf6e
stories2 <stories2@naver.com>
2017-08-14 11:15:04 +0900

    DataTransfer module : convert get data to json

--- Utils/DataTransfer.js
+++ Utils/DataTransfer.js
@@ -3,7 +3,24 @@ function DataTransferManager() {
 }
 
 DataTransferManager.prototype.CustomJsonToOriginalJson = function (customJsonData) {
-    
+
+    PrintLogMessage("DataTransfer", "CustomJsonToOriginalJson", "preparing parse custom json data", LOG_LEVEL_INFO)
+    var jsonDatas = {}
+
+    splitedMessage = customJsonData.split("&")
+
+    for(indexOfDataNumber in splitedMessage) {
+        keyValueData = splitedMessage[indexOfDataNumber].split("=")
+
+        key = keyValueData[0]
+        value = keyValueData[1]
+
+        jsonDatas[key] = value
+
+        PrintLogMessage("DataTransfer", "CustomJsonToOriginalJson", "key: " + key + " value: " + value, LOG_LEVEL_INFO)
+    }
+    // PrintLogMessage("DataTransfer", "CustomJsonToOriginalJson", "result json: " + jsonDatas, LOG_LEVEL_INFO)
+    return jsonDatas
 } 
 
 DataTransferManager.prototype.CommunicateWithBackend = function (url, originalJsonRequestData, callbackReciveObject) {
