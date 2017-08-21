a229eb51236f532ec78a88ee2a283e1f812c7800
stories2 <stories2@naver.com>
2017-08-21 18:45:31 +0900

    ConfigLoader module : load config text from config.txt

--- Utils/ConfigLoader.js
+++ Utils/ConfigLoader.js
@@ -1,14 +1,25 @@
 function ConfigLoader() {
     PrintLogMessage("ConfigLoader", "ConfigLoader", "Init config loader", LOG_LEVEL_INFO);
-    this.LoadConfigFile()
+    this.configFileText = ""
+    this.LoadConfigFile(this)
 }
 
-ConfigLoader.prototype.LoadConfigFile = function () {
+ConfigLoader.prototype.LoadConfigFile = function (configFileLoaderManager) {
     PrintLogMessage("ConfigLoader", "LoadConfigFile", "load config file", LOG_LEVEL_INFO);
+    var configInfoCrawler = new XMLHttpRequest();
+    configInfoCrawler.open('GET', PHP_DESTINATION + '/config.txt');
+    configInfoCrawler.onreadystatechange = function() {
+        PrintLogMessage("ConfigLoader", "LoadConfigFile", "responsed get config file text", LOG_LEVEL_INFO);
+        this.configFileText = configInfoCrawler.responseText
+        configFileLoaderManager.ParseConfigFile(this.configFileText)
+    }
+    configInfoCrawler.send();
+    PrintLogMessage("ConfigLoader", "LoadConfigFile", "request get config file text", LOG_LEVEL_INFO);
 }
 
-ConfigLoader.prototype.ParseConfigFile = function () {
+ConfigLoader.prototype.ParseConfigFile = function (configFileText) {
     PrintLogMessage("ConfigLoader", "ParseConfigFile", "parse config file", LOG_LEVEL_INFO);
+    
 }
 
 ConfigLoader.prototype.GetIndexOfConfigData = function (key) {
